107dec3ad87af8314ba58cadda2f3bcfbec8cc97
kevinlau001 <laukevin@ymail.com>
2017-01-31 14:46:22 -0600

    Update example.pp

--- site/examples/files/fundamentals/example.pp
+++ site/examples/files/fundamentals/example.pp
@@ -1,12 +1,17 @@
+node default {
+  notify { "Hello World\n":}
+
 file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
   content => "Hey, Puppet is fun!\n",
 }
 
 package { 'cowsay':
-  ensure   => present,
+  ensure => present,
   provider => gem,
+}
+
 }
